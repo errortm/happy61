0261769e8f5f8162c7add88a231460a53fd219ec
Tang Min <tangmin@Tangs-Laptop.local>
2025-05-31 13:25:11 +0800

    fix: 后端端口改为3000，适配云托管

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,7 +5,7 @@ import uploadRouter from './routes/upload';
 import path from 'path';
 
 const app = express();
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
